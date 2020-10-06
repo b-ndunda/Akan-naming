49acac7a7d8a2e9a74708104da9335ecdea37900
b-ndunda <brndundakenya@gmail.com>
2020-10-06 09:17:36 +0300

    added image

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+// Declaring variables for female and male names.
 var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-
 // Create a function that will be called whenever you hit the submit button.
 function akanNames(){
     // Declare date inputs and get its values.
@@ -26,5 +26,5 @@ function akanNames(){
         document.getElementById("display").innerHTML="Your Akan Name is "+femaleNames[dayOfWeek];
     }else{
         document.getElementById("display").innerHTML="Your Akan Name is "+maleNames[dayOfWeek];
-    };
-    
+    }
+}
